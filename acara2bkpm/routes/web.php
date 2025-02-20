4840941d8678a4c41866332e8cd784851fb8eef9
Pradimarhm <pradanaivan574@gmail.com>
2025-02-20 15:25:06 +0700

    upload kedua

--- acara2bkpm/routes/web.php
+++ acara2bkpm/routes/web.php
@@ -5,3 +5,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('foo',function(){
+    return 'hello world';
+});
+
+Route::get('user/{id}',function($id){
+    return 'User'.$id;
+});
+
+Route::get('posts/{post}/comments/{comment}',function($postId, $commentId){
+
+});
+
+Route::get('/user', '/UserController@index');
+
+Route::match(['get','post'],'/',function(){
+
+});
+
+Route::any('/',function(){
+
+});
+
+
+
