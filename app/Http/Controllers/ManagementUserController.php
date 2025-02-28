26a958752b8a6914525eb0325466ad4da3c4d3c9
Pradimarhm <pradanaivan574@gmail.com>
2025-02-28 10:29:22 +0700

    acara 5 6

---
+++ app/Http/Controllers/ManagementUserController.php
@@ -0,0 +1,36 @@
+<?php
+//acara 5
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class ManagementUserController extends Controller
+{
+    public function index(){
+        return "Halo deck, method ini digunakan untuk mengambil semua data user";
+    }
+
+    public function create(){
+        return "Halo deck, method ini digunakan untuk menampilkan form untuk menambah data user";
+    }
+
+    public function store(){
+        return "Halo deck, method ini digunakan untuk menciptakan data user terbaru";
+    }
+    public function show($id){
+        return "Halo deck, method ini digunakan untuk mengambil satu data user dengan id=".$id;
+    }
+    public function edit($id){
+        return "Halo deck, method ini digunakan untuk menampilkan form untuk mengubah data edit dengan id=".$id;
+    }
+    public function update(Request $request, $id){
+        return "Halo deck, method ini digunakan untuk mengubah data user dengan id=".$id;
+    }
+    public function destroy($id){
+        return "Halo deck, method ini digunakan untuk menghapus data user dengan id=".$id;
+    }
+    
+}
+
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+//acara 5
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ManagementUserController;
+
+// Route untuk mengambil semua user
+Route::get('/user', [ManagementUserController::class, 'index']);
+
+// Route untuk menampilkan form tambah user
+Route::get('/user/create', [ManagementUserController::class, 'create']);
+
+// Route untuk menyimpan data user
+Route::post('/user', [ManagementUserController::class, 'store']);
+
+// Route untuk menampilkan satu user berdasarkan id
+Route::get('/user/{id}', [ManagementUserController::class, 'show']);
+
+// Route untuk menampilkan form edit user
+Route::get('/user/{id}/edit', [ManagementUserController::class, 'edit']);
+
+// Route untuk update data user
+Route::put('/user/{id}', [ManagementUserController::class, 'update']);
+
+// Route untuk menghapus data user
+Route::delete('/user/{id}', [ManagementUserController::class, 'destroy']);
+
+
+
+
+
